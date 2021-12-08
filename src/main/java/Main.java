f7df65178e15b712199dc9880c76f4289a824692
00011069 <Apulatjonov@gmail.com>
2021-12-08 12:20:46 +0500

    Changes by HURSHIDBEK

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,6 @@ import auxiliary.FrontEnd;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World!");
-        System.out.println("edited");
-        System.out.println("edited2");
-
-        System.out.println("eCommerce/08/12");
+        new FrontEnd().start();
     }
 }
